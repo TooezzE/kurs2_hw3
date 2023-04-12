d42e9932aa4593b82e7b42724463c57e4de28935
Егор <azazaz.com534@gmail.com>
2023-04-12 18:28:38 +0300

    homework 3 done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehicle bicycle = new Bicycle("Highway Riding Bicycle", 2);
+        Vehicle bicycle2 = new Bicycle("Mountain Riding Bicycle", 2);
+        Vehicle car = new Car("Sport Car", 4);
+        Vehicle car2 = new Car("Family Car", 4);
+        Vehicle truck = new Truck("Monster Truck", 8);
+        Vehicle truck2 = new Truck("Cargo Truck", 10);
+
+
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.check(bicycle);
+        serviceStation.check(bicycle2);
+        serviceStation.check(car);
+        serviceStation.check(car2);
+        serviceStation.check(truck);
+        serviceStation.check(truck2);
     }
 }
